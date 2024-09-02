6c56d8a68852e0202f78db853b4c0027bcce8aea
madyanes <madyanes.dev@gmail.com>
2024-09-02 16:29:13 +0700

    Fix: Optimize playSound with useCalback
    Optimize playSound with useCallback to prevent unnecessary useEffect calls
    
    - Wrapped playSound in useCallback to ensure it doesn't cause unnecessary rerenders.
    - Prevents useEffect from being triggered unnecessarily, which was causing manual changes to duration to be overwritten.
    - Ensures useEffect only runs when relevant dependencies (number, sets, speed, durationBreak) change.
    
    Tanpa useCallback untuk playSound:
    
    Setiap kali komponen Calculator melakukan rerender, fungsi playSound dibuat ulang sebagai fungsi baru di memori.
    
    Karena playSound adalah dependensi dari useEffect, setiap kali playSound dibuat ulang, useEffect menganggap ada perubahan pada dependensinya dan dijalankan ulang.
    
    Hal ini bisa menyebabkan useEffect dijalankan lebih sering dari yang diperlukan, dan akhirnya setDuration dipanggil lagi, yang mungkin menimpa perubahan yang baru saja dibuat oleh handleInc atau handleDec.
    
    Dengan useCallback untuk playSound:
    
    playSound hanya akan dibuat ulang jika ada perubahan pada dependensi yang didefinisikan di dalam useCallback.
    
    Jika tidak ada perubahan pada dependensinya, fungsi playSound yang sama digunakan pada rerender berikutnya, sehingga tidak ada perubahan pada array dependensi useEffect.
    
    Ini berarti useEffect hanya akan dijalankan ulang saat dependensi lain seperti number, sets, speed, atau durationBreak berubah, bukan hanya karena playSound dibuat ulang.

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from 'react'
+import { memo, useCallback, useEffect, useState } from 'react'
 import clickSound from './ClickSound.m4a'
 
 function Calculator({ workouts, allowSound }) {
@@ -9,26 +9,32 @@ function Calculator({ workouts, allowSound }) {
 
   const [duration, setDuration] = useState(0)
 
+  const playSound = useCallback(
+    function () {
+      if (!allowSound) return
+      const sound = new Audio(clickSound)
+      sound.play()
+    },
+    [allowSound]
+  )
+
   useEffect(() => {
     setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak)
-  }, [number, sets, speed, durationBreak])
+    playSound()
+  }, [number, sets, speed, durationBreak, playSound])
 
   // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak
   const mins = Math.floor(duration)
   const seconds = (duration - mins) * 60
 
-  const playSound = function () {
-    if (!allowSound) return
-    const sound = new Audio(clickSound)
-    sound.play()
-  }
-
   function handleInc() {
     setDuration((duration) => Math.floor(duration) + 1)
+    playSound()
   }
 
   function handleDec() {
     setDuration((duration) => (duration > 1 ? Math.ceil(duration) - 1 : 0))
+    playSound()
   }
 
   return (
